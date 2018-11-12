f94813fc786ec14c9ae4ff9ca879b4ac458e7e19
Volkov Sergey <truefunrun@hotmail.com>
2018-11-12 03:59:07 +0300

    feat: update lock files; feat: project logo image; feat: webpack build

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+module.exports = {
+  entry: ['./lib/kinka.js'],
+  mode: 'production',
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'kinka.min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  //   optimization: {
+  //     minimizer: [
+  //       new UglifyJsPlugin({
+  //         cache: true,
+  //         parallel: true,
+  //         uglifyOptions: { output: { comments: false } },
+  //       }),
+  //     ],
+  //   },
+}
