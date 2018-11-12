4e06d15143639e578952f7b2df7926523c4e9cf0
Volkov Sergey <truefunrun@hotmail.com>
2018-11-12 04:18:05 +0300

    revert: return importing production file in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/kinka.min.js')
-} else {
-  module.exports = require('./lib/kinka.js')
-}
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./dist/kinka.min.js')
+// } else {
+module.exports = require('./lib/kinka.js')
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'kinka.min.js',
+    library: 'kinka',
   },
   module: {
     rules: [
