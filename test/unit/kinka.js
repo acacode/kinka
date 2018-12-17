acf088e1366aa24dfe4f1a4bfdaf76a5b576522c
Volkov Sergey <truefunrun@hotmail.com>
2018-12-17 21:55:06 +0300

    fix: fix bug with options http method; test: update unit tests, added test on instance of Promise of each basic method

--- test/unit/kinka.js
+++ test/unit/kinka.js
@@ -3,12 +3,12 @@ import { expect } from 'chai'
 import { describe, it } from 'mocha'
 import kinka from '../..'
 
-describe('kinka', () => {
+describe('kinka instance', () => {
   it('should be defined', () => {
     expect(!!kinka).to.be.equal(true)
   })
 
-  describe('kinka basic methods', () => {
+  describe(' - kinka basic methods', () => {
     const examples = [
       'delete',
       'get',
@@ -18,10 +18,14 @@ describe('kinka', () => {
       'post',
       'put',
     ]
-    const test = name =>
-      it(`should have basic method ${name}`, () => {
+    const test = name => {
+      it(`should have basic method "${name}"`, () => {
         expect(typeof kinka[name]).to.be.equal('function')
       })
+      it(`basic method "${name}" should return <Promise> instance`, () => {
+        expect(kinka[name]('/test') instanceof Promise).to.be.equal(true)
+      })
+    }
     examples.forEach(test)
   })
 })
