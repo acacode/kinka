d8fea85ef80f14a7c5ba5071995fe5e687026680
Volkov Sergey <truefunrun@hotmail.com>
2018-12-17 21:39:27 +0300

    test: added a couple unit tests for kinka instance

--- test/unit/kinka.js
+++ test/unit/kinka.js
@@ -5,6 +5,23 @@ import kinka from '../..'
 
 describe('kinka', () => {
   it('should be defined', () => {
-    expect(!!kinka).to.be.true()
+    expect(!!kinka).to.be.equal(true)
+  })
+
+  describe('kinka basic methods', () => {
+    const examples = [
+      'delete',
+      'get',
+      'head',
+      'options',
+      'patch',
+      'post',
+      'put',
+    ]
+    const test = name =>
+      it(`should have basic method ${name}`, () => {
+        expect(typeof kinka[name]).to.be.equal('function')
+      })
+    examples.forEach(test)
   })
 })
