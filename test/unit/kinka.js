9d8737a2199ac18c573038510d7f744bfb0cb1c9
Volkov Sergey <truefunrun@hotmail.com>
2018-12-19 02:51:09 +0300

    test: filled up tests for some things in kinka.js

--- test/unit/kinka.js
+++ test/unit/kinka.js
@@ -10,8 +10,9 @@ describe('kinka instance : ', () => {
     expect(!!kinka).to.be.equal(true)
   })
 
-  const testMethod = (name, api = kinka) => {
-    const path = '/test'
+  const testMethod = (name, options = {}) => {
+    const api = options.api || kinka
+    const path = (options.baseURL || '') + '/test'
     describe(` - ${name} method : `, function() {
       let onCreateXHRstub = sinon.stub()
       beforeEach(() => {
@@ -33,38 +34,97 @@ describe('kinka instance : ', () => {
         expect(onCreateXHRstub.calledOnce).to.equal(true)
         done()
       })
-      it(`should create request with ${name.toUpperCase()} method`, function(done) {
+      it(`request should have ${name.toUpperCase()} method`, function(done) {
         global.XMLHttpRequest.onSend = function(xhr) {
           expect(xhr.method).to.be.equal(name.toUpperCase())
           done()
         }
         api[name](path)
       })
-      it(`should create request with "/test" url`, function(done) {
+      it(`request should have "${path}" url`, function(done) {
         global.XMLHttpRequest.onSend = function(xhr) {
           expect(xhr.url).to.be.equal(path)
           done()
         }
         api[name](path)
       })
-      // it(`should create request with "/test" url`, function(done) {
-      //   global.XMLHttpRequest.onSend = function(xhr) {
-      //     expect(xhr.url).to.be.equal('/url')
-      //     done()
-      //   }
-      //   api[name](path)
-      // })
     })
   }
 
-  describe(' - kinka basic methods : ', () => {
+  describe(' - basic methods : ', () => {
     const methods = ['delete', 'get', 'head', 'options', 'patch', 'post', 'put']
     methods.forEach(name => testMethod(name))
   })
 
-  describe(' - kinka custom methods', () => {
+  describe(' - custom methods : ', () => {
     const customMethods = ['permanent', 'kill', 'stop', 'move']
     const api = kinka.create({ customMethods })
-    customMethods.forEach(name => testMethod(name, api))
+    customMethods.forEach(name => testMethod(name, { api }))
+  })
+  describe(' - using baseURL option : ', () => {
+    const examples = [
+      ['https://test-server.com', 'https://test-server.com'],
+      ['test-server.com', 'test-server.com'],
+      ['//test-server.com', '//test-server.com'],
+      ['ws://test-server.com', 'ws://test-server.com'],
+      ['http://localhost:8080', undefined, 'http://localhost:8080'],
+      ['', ''],
+      ['http://localhost:6060', '', 'http://localhost:6060'],
+    ]
+    const test = ([expected, baseURL, locationOrigin]) => {
+      it(`kinka instance should contains "${expected}" baseURL`, () => {
+        global.location = locationOrigin
+          ? { origin: locationOrigin }
+          : undefined
+        const api = kinka.create({ baseURL })
+        expect(api.baseURL).to.be.equal(expected)
+      })
+    }
+    examples.forEach(test)
+    describe(' - requests : ', () => {
+      const examples = [
+        [
+          'https://test-server.com/example',
+          'https://test-server.com',
+          '/example',
+        ],
+        ['test-server.com/example', 'test-server.com', '/example'],
+        ['//test-server.com/test', '//test-server.com', '/test'],
+        [
+          'ws://test-server.com/websocket',
+          'ws://test-server.com',
+          '/websocket',
+        ],
+        [
+          'http://localhost:8080/example',
+          undefined,
+          '/example',
+          'http://localhost:8080',
+        ],
+        ['/example', '', '/example'],
+        [
+          'http://localhost:6060/example',
+          '',
+          '/example',
+          'http://localhost:6060',
+        ],
+      ]
+      const test = ([expected, baseURL, path, locationOrigin]) => {
+        const method = 'get'
+        it(`request should have "${expected}" url`, function(done) {
+          global.location = locationOrigin
+            ? { origin: locationOrigin }
+            : undefined
+          global.XMLHttpRequest = MockXMLHttpRequest.newMockXhr()
+          const api = kinka.create({ baseURL })
+          global.XMLHttpRequest.onSend = function(xhr) {
+            expect(xhr.url).to.be.equal(expected)
+            done()
+          }
+          api[method](path)
+        })
+      }
+      examples.forEach(test)
+    })
   })
 })
