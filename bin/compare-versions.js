51642f51315b8c59c61f361fa8c4a67f862464c0
Volkov <truefunrun@hotmail.com>
2018-12-20 18:01:15 +0300

    fix: short: remove unused constant variable

--- bin/compare-versions.js
+++ bin/compare-versions.js
@@ -5,8 +5,9 @@ function compareVersions(packageName) {
   spawnCommand(`npm view ${packageName} version`, npmVersion => {
     const versionsAreEqual = currentVersion === npmVersion
     if (versionsAreEqual) {
-      const errorMessage = `Current version (${currentVersion}) should be updated/changed (npm: ${npmVersion})`
-      throw Error(errorMessage)
+      throw Error(
+        `Current version (${currentVersion}) should be updated/changed (npm: ${npmVersion})`
+      )
     } else {
       console.log(
         `Current version (${currentVersion}) is not equals with npm version (npm: ${npmVersion}). That's fine!`
