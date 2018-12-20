5eb4ddce85a26deb7b07f14a921ee4f5e3ee90be
Volkov <truefunrun@hotmail.com>
2018-12-20 17:56:16 +0300

    feat: updated compare-versions script, added create-release, spawn-command scripts

--- bin/compare-versions.js
+++ bin/compare-versions.js
@@ -1,44 +1,17 @@
-const proc = require('child_process')
-const isWin = require('os').platform() === 'win32'
+const spawnCommand = require('./spawn-command')
 const currentVersion = require('../package.json').version
 
-function getCommandLine(task, env) {
-  var file, args
-  var options = {
-    stdio: 'pipe',
-    windowsVerbatimArguments: isWin,
-    env,
-  }
-  if (isWin) {
-    file = process.env.comspec || 'cmd.exe'
-    args = ['/s', '/c', task]
-  } else {
-    file = '/bin/sh'
-    args = ['-c', task]
-  }
-  return [file, args, options]
-}
-
 function compareVersions(packageName) {
-  const procOptions = getCommandLine(
-    `npm view ${packageName} version`,
-    process.env
-  )
-  const command = proc.spawn.apply(proc, procOptions)
-
-  command.stdout.on('data', message => {
-    const npmVersion = message.toString().replace('\n', '')
+  spawnCommand(`npm view ${packageName} version`, npmVersion => {
     const versionsAreEqual = currentVersion === npmVersion
     if (versionsAreEqual) {
       const errorMessage = `Current version (${currentVersion}) should be updated/changed (npm: ${npmVersion})`
       throw Error(errorMessage)
     } else {
       console.log(
-        "Current version is not equals with npm version. That's fine!"
+        `Current version (${currentVersion}) is not equals with npm version (npm: ${npmVersion}). That's fine!`
       )
     }
-    command.kill('SIGINT')
-    command.kill('SIGTERM')
     process.exit(+versionsAreEqual)
   })
 }
