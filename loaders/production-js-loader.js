fba5f47cdd63e4f029c63027c6221f76b6296a85
Sergey Volkov <servolk@mera.ru>
2019-01-21 14:35:15 +0300

    feat: added type checks for custom methods; fix: reg exp for procces env production in custom js loader

--- loaders/production-js-loader.js
+++ loaders/production-js-loader.js
@@ -1,8 +1,8 @@
 module.exports = function(source) {
   return source
-    .replace(/typeCheck\([\w\r\n., ']+\)/g, '')
+    .replace(/typeCheck\([\w\r\n., '[\]+]+\)/g, '')
     .replace(
-      /if \(process.env.NODE_ENV !== 'production'\) {[\w\r\n., /*@{}|!&=;()[\]+']+(\}\n)/g,
+      /if \(process.env.NODE_ENV !== 'production'\) {[\w\r\n., /*@{}|!&=;()[\]+'?:]+(\}\n)/g,
       ''
     )
     .replace(/emptyCheck\([\w\r\n., ']+\)/g, '')
