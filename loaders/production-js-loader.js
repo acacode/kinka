e53f49a45ce37d13a7ce46d50bc924229a36e848
Sergey Volkov <servolk@mera.ru>
2019-01-21 13:49:58 +0300

    feat: production js loader added removing typeCheck function

--- loaders/production-js-loader.js
+++ loaders/production-js-loader.js
@@ -1,3 +1,8 @@
 module.exports = function(source) {
-  return source.replace(/typeCheck\([\w\r\n., ']+\)/g, '')
+  return source
+    .replace(/typeCheck\([\w\r\n., ']+\)/g, '')
+    .replace(
+      /if \(process.env.NODE_ENV !== 'production'\) {[\w\r\n., /*@{}|!&=;()[\]+']+(\}\n)/g,
+      ''
+    )
 }
