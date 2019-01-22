c69ba5c9c24bc8f59f5b64f95464d192d39dba25
Sergey Volkov <servolk@mera.ru>
2019-01-22 20:58:37 +0300

    fix: production js loader

--- loaders/production-js-loader.js
+++ loaders/production-js-loader.js
@@ -2,18 +2,18 @@ module.exports = function(source) {
   const prodSource = source
     .replace(/typeCheck\([\w\r\n., '[\]+]+\)/g, '')
     .replace(
-      /if \(process.env.NODE_ENV !== 'production'\) {[\w\r\n., /*@{}|!&=;()[\]+'?:]+(\}\n)/g,
+      new RegExp('(// <non-prod-code>)[^]*(// </non-prod-code>)', 'g'),
       ''
     )
     .replace(/emptyCheck\([\w\r\n., '[\]+]+\)/g, '')
-  // if (
-  //   prodSource.indexOf('typeCheck') > -1 ||
-  //   prodSource.indexOf('emptyCheck') > -1
-  // ) {
-  //   console.log('see source ', prodSource)
-  //   throw Error(
-  //     'Occured an error linked with removing typeCheck and emptyCheck function from source file'
-  //   )
-  // }
+  if (
+    prodSource.indexOf('typeCheck') > -1 ||
+    prodSource.indexOf('emptyCheck') > -1
+  ) {
+    console.log('see source ', prodSource)
+    throw Error(
+      'Occured an error linked with removing typeCheck and emptyCheck function from source file'
+    )
+  }
   return prodSource
 }
