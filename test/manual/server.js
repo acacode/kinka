dad9b0343e838d772cdf1e8a2c64bceae2257698
Volkov Sergey <truefunrun@hotmail.com>
2019-01-24 01:06:15 +0300

    test: created sketches for manual testing

---
+++ test/manual/server.js
@@ -0,0 +1,7 @@
+// const path = require('path')
+// const express = require('express')
+// const config = require('../../webpack.config.js')
+
+// const webpack = require('webpack')
+
+// console.log(webpack(config))
