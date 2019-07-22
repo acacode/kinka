74394ef1049bbb3fbab2709d0116851d626d32a0
Volkov Sergey <truefunrun@hotmail.com>
2019-07-22 09:08:07 +0300

    fix: travis ci config; chore: try to fix manual tests

--- test/manual/server.js
+++ test/manual/server.js
@@ -1,10 +1,10 @@
 const path = require('path')
 const fs = require('fs')
 const express = require('express')
-const configs = require('../../webpack.config.js')
+// const configs = require('../../webpack.config.js')
 
 const app = express()
-const webpack = require('webpack')
+// const webpack = require('webpack')
 const PORT = 7077
 
 const resolve = dir => path.resolve(__dirname, dir)
@@ -14,19 +14,23 @@ fs.readFile(resolve('build.html'), 'utf8', (err, buildTemplate) => {
 
   app.use(express.static(resolve('../../dist')))
 
-  configs.forEach(config => {
-    const publicPath = ''
-    const filename = config.output.filename
-    config.output.publicPath = publicPath
-    app.use(
-      require('webpack-dev-middleware')(webpack(config), {
-        noInfo: true,
-        publicPath: publicPath,
-      })
-    )
-    app.get('/' + filename.replace('.js', '.html'), function(req, res) {
-      res.send(buildTemplate.replace(/({{build-file-name}})/g, filename))
-    })
+  // TODO: complete manual testing
+  // configs.forEach(config => {
+  //   const publicPath = ''
+  //   const filename = config.output.filename
+  //   config.output.publicPath = publicPath
+  //   // app.use(
+  //   //   require('webpack-dev-middleware')(webpack(config), {
+  //   //     noInfo: true,
+  //   //     publicPath: publicPath,
+  //   //   })
+  //   // )
+  //   app.get('/' + 'kinka.js', function(req, res) {
+  //     res.send(buildTemplate.replace(/({{build-file-name}})/g, filename))
+  //   })
+  // })
+  app.get('/' + 'kinka.js', function(req, res) {
+    res.send(buildTemplate.replace(/({{build-file-name}})/g, 'kinka.js'))
   })
 
   app.get('/test.js', function(req, res) {
